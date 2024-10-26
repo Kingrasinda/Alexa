f444d89831f12cf34675fb54699f292a1ffc04d9
Kingrasinda <kingrasinda@gmail.com>
2024-10-27 03:12:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "GDQxHaaZ#61F7KI2LrusWR7Ed5qqfg762oiKfZBTRaZcwJrZWoPg",
+SESSION_ID: process.env.SESSION_ID || "KHgE0ALa#a_OscjZoaWxtJyDD-wJSKglzzmV6rT5ttPqP64dNbnw",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
