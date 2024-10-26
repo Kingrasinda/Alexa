391c52446ac0e8506a84182733028f21793866c0
Kingrasinda <kingrasinda@gmail.com>
2024-10-27 01:57:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RfUziAZQ#xmtAtCLYtdDqag19Pw20MFedNbHQEWnxz7WYNIstDYc",
+module.exports = {GDQxHaaZ#61F7KI2LrusWR7Ed5qqfg762oiKfZBTRaZcwJrZWoPg
+SESSION_ID: process.env.SESSION_ID || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
